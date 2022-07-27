461a9efe7fdb2d5c5099b5705b4bb2940ad14164
Esengul Cinel <crl.cinel00@gmail.com>
2022-07-27 21:14:31 +0300

    DairanınAlanıCevresi

---
+++ src/Giris/DairanınAlanıCevresi.java
@@ -0,0 +1,16 @@
+package Giris;
+import java.util.Scanner;
+public class DairanınAlanıCevresi {
+    public static void main(String[] args) {
+        int r;
+        double pi=3.14,alan,cevre;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Yaricapi giriniz:");
+        r=input.nextInt();
+        alan=pi*r*r;
+        cevre=2*pi*r;
+        System.out.println("alanı:"+alan);
+        System.out.println("\ncevresi:"+cevre);
+
+    }
+}
